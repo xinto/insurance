48324dd3cf2d6a5aea77bda6097651e572287535
Juan Carlos Breton Quintero <sins_alltime@hotmail.com>
2013-06-17 18:06:22 -0500

    I add several payment methods

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -19,8 +19,8 @@ class BootStrap {
 
     void createAdminUserIfRequired() {
     	def paymentMethod = new PaymentMethod(name:"Debito").save()
-    	def paymentMethod = new PaymentMethod(name:"Tarjeta de Credito").save()
-    	def paymentMethod = new PaymentMethod(name:"Tarjeta de Debito").save()
+    	def paymentMethod1 = new PaymentMethod(name:"Tarjeta de Credito").save()
+    	def paymentMethod2 = new PaymentMethod(name:"Tarjeta de Debito").save()
     	def currency = new Currency(name: "Udis",
     							         description:"Moneda utilizada en el sector de seguros de vida").save()
     	def paymentForm = new PaymentForm(name:"Mensual").save()
